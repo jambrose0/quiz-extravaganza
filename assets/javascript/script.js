88e67906b5224e5c7b27d13d31ee56bca2133bb6
jambrose0 <jambrose0518@gmail.com>
2022-01-30 15:46:23 -0500

    renaming variables and adding dom elements to javascript

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,134 +1,123 @@
-//global variables
+//dom elements
+var timer = document.getElementById("timer");
+var timeLeft = document.getElementById("timeLeft");
+var timeDone = document.getElementById("timeDone");
+var startButton = document.getElementById("start-button");
 var quizConatiner = document.getElementById("quiz");
 var resultsContainer = document.getElementById("results");
-var submitButton = document.getElementById("submit");
+var submitQuiz = document.getElementById("submitQuiz");
+
+var initials = document.getElementById("initials");
+
+//global variables
 var startTimer = setInterval(startTimer, "click");
-var quizTracker = 0;
+var questionEl = 0;
+var allTime = 180;
 
-// timer on button click function
-function startTimer() {
-  // var countdown =
-}
-//button to start quiz
-var startButton = document.getElementById("start");
-startButton.addEventListener("click", startQuiz);
-
-//start quiz function
+//start timer on start button click
 function startQuiz() {
-  //   var question = `<h3>${quizQuestions[quizTracker].question}</h3>`;
-  //   var answers = `<ul><li class = 'quizAnswers'>${quizQuestions[quizTracker].answers.a}</li><li>${quizQuestions[quizTracker].answers.b}</li>
-  //   <li>${quizQuestions[quizTracker].answers.c}</li><li>${quizQuestions[quizTracker].answers.d}</li></ul>`;
+  questionEl = 0;
+  allTime = 180;
+  timeLeft.textContent = time;
+  initials.textContent = "";
 
-  var answerStuf = document.createElement("ul");
-  for (var key in quizQuestions[quizTracker].answers) {
-    var answerShape = document.createElement("li");
-    answerShape.textContent = quizQuestions[quizTracker].answers[key];
-    //   answerShape.addEventListener('click',)
-    answerStuf.append(answerShape);
-  }
-  quizConatiner.innerHTML = `${question} ${answers}`;
+  var startTimer = setInterval(function () {
+    allTime--;
+    timeLeft.textContent = allTime;
+    if (allTime <= 0) {
+      clearInterval(startTimer);
+      if (questionEl < quizQuestions.length - 1) {
+        endQuiz();
+      }
+    }
+  }, 1000);
 
-  quizConatiner.appendChild(answerStuf);
+  quizGo();
 }
 
-//quiz questions
+function quizGo() {
+  startQuestions();
+}
+
+function startQuestions() {
+  var answerEl = document.createElement("ul");
+  for (var key in quizQuestions[quest].answers) {
+    var answerShape = document.createElement("li");
+    answerShape.textContent = quizQuestions[questionEl].answers[key];
+    //   answerShape.addEventListener('click',)
+    answerEl.append(answerShape);
+  }
+  quizContainer.innerHTML = `${question} ${answers}`;
+
+  quizContainer.appendChild(answerEl);
+}
+
+function answerReview(correct) {
+  if (quizQuestions[questionEl].correct === quizQuestions[questionEl].)
+}
+
+
+
+//quiz question array
 var quizQuestions = [
   {
     question: "Which is one of my dogs names?",
-    answers: {
-      a: "Rufus",
-      b: "Squidward",
-      c: "Shadow",
-      d: "Fido",
-    },
+    answers: [
+      "a: Rufus",
+      "b: Squidward",
+      'c: Shadow',
+      'd: "Fido'
+  ],
     correct: "c",
   },
   {
     question: "What is the name of my second dog?",
-    answers: {
-      a: "Isabelle",
-      b: "Ranger",
-      c: "Feather",
-      d: "Scout",
-    },
+    answers: [
+      "a: Isabelle",
+      "b: Ranger",
+      'c: Feather',
+      "d: Scout",
+    ],
     correct: "d",
   },
   {
     question: "What is my dogs' favorite activity?",
-    answers: {
-      a: "sleeping on the couch",
-      b: "fetch",
-      c: "hiking",
-      d: "barking at random noises",
-    },
+    answers: [
+      "a: sleeping on the couch",
+      "b: fetch",
+      "c: hiking",
+      "d: barking at random noises",
+    ],
     correct: "b",
   },
   {
     question: "What is Shadow's favorite snack",
-    answers: {
-      a: "whatever I'm eating at the moment",
-      b: "carrots",
-      c: "dog treats",
-      d: "scrambled eggs",
-    },
+    answers: [
+      "a: whatever I'm eating at the moment",
+      "b: carrots",
+      "c: dog treats",
+      "d: scrambled eggs",
+],
     correct: "a",
   },
   {
     question: "What color are both of my dogs?",
-    answers: {
-      a: "golden",
-      b: "light brown",
-      c: "black with white racing stripes",
-      d: "hypoallergenic- furless dogs",
-    },
+    answers: [
+      "a: golden",
+      "b: light brown",
+      "c: black with white racing stripes",
+      "d: hypoallergenic- furless dogs",
+    ],
     correct: "c",
   },
   {
     question: "Which of my dogs is older?",
-    answers: {
-      a: "Shadow",
-      b: "Scout",
-      c: "Rufus",
-      d: "Fido",
-    },
+    answers: [
+      " a: Shadow",
+      "b: Scout",
+      "c: Rufus",
+      "d: Fido",
+    ],
     correct: "a",
   },
 ];
-
-function buildQuiz() {
-  var output = [];
-
-  quizQuestions.forEach((currentQuestion, questionNumber) => {
-    var answers = [];
-
-    for (letter in quizQuestions.answers) {
-    }
-  });
-  quizConatiner.innerHTML = output.join("");
-}
-
-function showResults() {
-  var answerContainer = quizConatiner.querySelectorAll(".answers");
-
-  var numCorrect = 0;
-
-  quizQuestions.forEach((currentQuestion, questionNumber) => {
-    var answerContainer = answerContainers[questionNumber];
-    var selector = `input[name=question${questionNumber}]:checked`;
-    var userAnswer = (answerContainer.querySelector(selector) || {}).value;
-
-    if (userAnswer === currentQuestion.correctAnswer) {
-      numCorrect++;
-
-      answerContainers[questionNumber].style.color = "lightgreen";
-    } else {
-      answerContainers[questionNumber].style.color = "red";
-    }
-  });
-
-  resultsContainer.innerHTML = `${numCorrect} out of ${quizQuestions.length}`;
-}
-
-buildQuiz();
-
-submitButton.addEventListener("click", showResults);
