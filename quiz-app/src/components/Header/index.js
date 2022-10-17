2a884f32b12df0cced49f9eaedb2272362aeb9a2
jambrose0 <jambrose0518@gmail.com>
2022-10-17 08:51:01 -0400

    initial header component

---
+++ quiz-app/src/components/Header/index.js
@@ -0,0 +1,40 @@
+import * as React from 'react';
+// import './index.css';
+
+
+
+const Header = ()  => {
+
+return (
+<header className='header'>
+    <h3>
+        Quiz Builder
+    </h3>
+<nav className='navigation'>
+    <ul>
+        <li className='mx-2'>
+<a href='#login'>
+
+        login
+</a>
+        </li>
+    <li>
+        <a href='#signup'>
+
+        sign up
+        </a>
+    </li>
+    </ul>
+</nav>
+
+</header>
+);
+
+
+
+
+
+}
+
+
+export default Header;
